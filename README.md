d7f5f8cfd7748855dbfd54e6713b8ac8ce4a1fed
Bhautik Gondaliya <mr.genius0180@gmail.com>
2025-02-06 23:02:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sentiment_Analysis_project
+ This is a Flask-based web application for sentiment analysis. It takes user input in the form of text and predicts whether the sentiment is positive or negative using a trained classification model.
---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, render_template
+
+import re
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+
+import pickle
+
+tfidf = pickle.load(open('tfidf.pkl', 'rb'))
+clf_model = pickle.load(open('clf_model.pkl', 'rb'))
+
+
+app =Flask(__name__)
+
+
+#################### Clean Review ###################
+stop_word = set(stopwords.words("english"))
+emoji_pattern = re.compile(r'(?::|;|=)(?:-)?(?:\)|\(|D|P)')
+
+def preprocessing(text):
+    text = re.sub(r'<[^>]*>', '', text) # remove html tags
+    emojis = emoji_pattern.findall(text)
+    text = re.sub(r'[\W+]', ' ', text.lower()) + ' '.join(emojis).replace('-', '')
+
+    peter = PorterStemmer()
+    text = [peter.stem(word) for word in text.split() if word not in stop_word]
+
+    return " ".join(text)
+#####################################################
+
+
+############## call index.html file #################
+@app.route('/')
+def index():
+    return render_template("index.html")
+#####################################################
+
+
+################ Predict Sentiment ##################
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        review = request.form['text']
+        
+        cleaned_review = preprocessing(review)
+
+        review_vector = tfidf.transform([cleaned_review])
+        
+        predicted_sentiment = clf_model.predict(review_vector)[0]
+
+        return render_template('index.html', prediction=predicted_sentiment)
+#####################################################
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
